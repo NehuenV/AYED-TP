6b695eddaee8d266c187e1bc39bae4f8a17200d0
NehuenV <32799856+NehuenV@users.noreply.github.com>
2020-09-13 20:22:39 -0300

    entidad

---
+++ UNLA-TPAYED/entidades/Sucursal.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <sstream>
+
+#include "Sucursal.h"
+
+void crearSucursal(Sucursal &sucursal,int codSucursal,char provincia[20], int cantArticulo, float monto, int cm2,int casaMatriz ){
+    sucursal.codSucursal = codSucursal;
+    sucursal.provincia = provincia;
+    sucursal.cantArticulo = cantArticulo:
+    sucursal.monto = monto;
+    sucursal.cm2= cm2;
+    sucursal.casaMatriz = casaMatriz;
+
+}
+
+void borrarSucursal(Sucursal &sucursal)
+{
+}
+
+int getCodSucursal(Sucursal &sucursal){
+    return sucursal.codSucursal;
+}
+void setCodSucursal(Sucursal &sucursal, int codSucursal){
+    sucursal.codSucursal = codSucursal;
+}
+
+//char
+char * getProvincia(Sucursal &sucursal)
+{
+    return sucursal.provincia;
+}
+void setProvincia(Sucursal &sucursal, char provincia[20])
+{
+    sucursal.provincia = provincia;
+}
+
+int getCantArticulo(Sucursal &sucursal, int cantArticulo)
+{
+    return sucursal.cantArticulo;
+}
+void setCantArtciulo(Sucursal &sucursal, int cantArticulo)
+{
+    sucursal.cantArticulo = cantArticulo;
+}
+
+float getMonto(Sucursal &sucursal)
+{
+    return sucursal.monto;
+}
+void setMonto(Sucursal &sucursal, float monto)
+{
+    sucursal.monto = monto;
+}
+
+int getCm2(Sucursal &sucursal)
+{
+    return sucursal.cm2;
+}
+void setCm2(Sucursal &sucursal, int cm2)
+{
+    sucursal.cm2 = cm2;
+}
+
+int getCasaMatriz(Sucursal &sucursal)
+{
+    return sucursal.casaMatriz;
+}
+void setCasaMatriz(Sucursal &sucursal, int casaMatriz)
+{
+    sucursal.casaMatriz = CasaMatriz;
+}
---
+++ UNLA-TPAYED/entidades/Sucursal.h
@@ -0,0 +1,39 @@
+#ifndef SUCURSAL_H_INCLUDED
+#define SUCURSAL_H_INCLUDED
+
+typedef struct {
+    int codSucursal;
+    char provincia[20];
+    int cantArticulo;
+    float monto;
+    int cm2;
+    int casaMatriz;
+}Sucursal;
+
+void crearSucursal(Sucursal &sucursal,int codSucursal,char provincia, int cantArticulo, float monto, int cm2,int casaMatriz );
+
+void borrarSucursal(Sucursal &sucursal);
+
+int getCodSucursal(Sucursal &sucursal);
+void setCodSucursal(Sucursal &sucursal, int codSucursal);
+
+//char
+char * getProvincia(Sucursal &sucursal);
+void setProvincia(Sucursal &sucursal, char provincia[20]);
+
+int getCantArticulo(Sucursal &sucursal, int cantArticulo);
+void setCantArtciulo(Sucursal &sucursal, int cantArticulo);
+
+float getMonto(Sucursal &sucursal);
+void setMonto(Sucursal &sucursal, float monto);
+
+int getCm2(Sucursal &sucursal);
+void setCm2(Sucursal &sucursal, int cm2);
+
+int getCasaMatriz(Sucursal &sucursal);
+void setCasaMatriz(Sucursal &sucursal, int casaMatriz);
+
+
+
+
+#endif // SUCURSAL_H_INCLUDED
