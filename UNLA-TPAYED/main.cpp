c4d68fd75afc9fdd762bb592ecac03452265cc09
nehuen verges <nehuenverges@hotmail.com>
2020-09-21 16:07:15 -0300

    hay un bug en el ordenamiento de provincia/monto, corregir eso

--- UNLA-TPAYED/main.cpp
+++ UNLA-TPAYED/main.cpp
@@ -14,6 +14,14 @@ ResultadoComparacion compararMonto(PtrDato ptrDato1, PtrDato ptrDato2) {
     else
         return IGUAL;
 }
+ResultadoComparacion compararMontoProv(PtrDato ptrDato1, PtrDato ptrDato2) {
+    if (getProvincia(*(Sucursal*)ptrDato1)==getProvincia(*(Sucursal*)ptrDato2) &&getMonto(*(Sucursal*)ptrDato1) > getMonto(*(Sucursal*)ptrDato2))
+        return MAYOR;
+    else if (getProvincia(*(Sucursal*)ptrDato1)==getProvincia(*(Sucursal*)ptrDato2) && getMonto(*(Sucursal*)ptrDato1) < getMonto(*(Sucursal*)ptrDato2))
+        return MENOR;
+        if(getProvincia(*(Sucursal*)ptrDato1)==getProvincia(*(Sucursal*)ptrDato2) && getMonto(*(Sucursal*)ptrDato1) == getMonto(*(Sucursal*)ptrDato2)){
+            return IGUAL;}
+}
 ResultadoComparacion compararIdProvincia(PtrDato ptrDato1, PtrDato ptrDato2) {
     if (getId(*(SProvincia*)ptrDato1) < getId(*(SProvincia*)ptrDato2))
         return MAYOR;
@@ -22,24 +30,47 @@ ResultadoComparacion compararIdProvincia(PtrDato ptrDato1, PtrDato ptrDato2) {
     else
         return IGUAL;
 }
+void listasProvincias(Lista &listaSuc, Lista &listaProv, Lista listaOrdenProvinciasMonto)
+{
+    PtrNodoLista ptrCursorP = primero(listaProv);
+    PtrNodoLista ptrCursorS = primero(listaSuc);
+    while ( ptrCursorP != fin() ) {
+        while ( ptrCursorS != fin() ) {
+
+           if(getProvArch(*(SProvincia*)ptrCursorP->ptrDato)==getProvincia(*(Sucursal*)ptrCursorS->ptrDato)){
+                adicionarPrincipio(listaOrdenProvinciasMonto,(Sucursal*)ptrCursorS);
+           }
+            ptrCursorS=ptrCursorS->sgte;
+        }
+    }
+}
+
+
 
 int main()
 {
     cout<< "leyendo archivo"<<endl;
     Lista listaSuc;
-    crearLista(listaSuc,compararMonto);
+    crearLista(listaSuc,compararMontoProv);
     cargarSucursal( listaSuc);
     reordenar(listaSuc);
+    reordenar(listaSuc);
     ImprimirLista(listaSuc);
-
+/*
     Lista listaProvincias;
     crearLista(listaProvincias,compararIdProvincia);
     cargarProvincias(listaProvincias);
-    ImprimirListaProvincia(listaProvincias);
+   // ImprimirListaProvincia(listaProvincias);
 
-    eliminarListaProvincia(listaProvincias);
+    Lista listaOrdenProvinciasMonto;
+    crearLista(listaOrdenProvinciasMonto,compararMonto);
+*/
+
+   // eliminarListaProvincia(listaProvincias);
     eliminarListaSucursal(listaSuc);
 
 
     return 0;
 }
+
+
