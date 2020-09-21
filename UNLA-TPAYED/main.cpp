7d2341938537a3a1c940c4dbe342762623f90cf7
nehuen verges <nehuenverges@hotmail.com>
2020-09-21 19:51:46 -0300

    no se que fue el ultimo cambio pero aca esta

--- UNLA-TPAYED/main.cpp
+++ UNLA-TPAYED/main.cpp
@@ -23,11 +23,11 @@ ResultadoComparacion compararMonto(PtrDato ptrDato1, PtrDato ptrDato2) {
         return IGUAL;
 }
 ResultadoComparacion compararMontoProv(PtrDato ptrDato1, PtrDato ptrDato2) {
-    if (getProvincia(*(Sucursal*)ptrDato1)==getProvincia(*(Sucursal*)ptrDato2))
+    if (getProvincia(*(Sucursal*)ptrDato1)>getProvincia(*(Sucursal*)ptrDato2))
     {
         return MAYOR;
     }else
-    if (getProvincia(*(Sucursal*)ptrDato1)!=getProvincia(*(Sucursal*)ptrDato2))
+    if (getProvincia(*(Sucursal*)ptrDato1)<getProvincia(*(Sucursal*)ptrDato2))
     {
         return MENOR;
     }
@@ -36,11 +36,11 @@ ResultadoComparacion compararMontoProv(PtrDato ptrDato1, PtrDato ptrDato2) {
 }
 
 ResultadoComparacion compararArticuloProv(PtrDato ptrDato1, PtrDato ptrDato2) {
-    if (getProvincia(*(Sucursal*)ptrDato1)==getProvincia(*(Sucursal*)ptrDato2))
+    if (getProvincia(*(Sucursal*)ptrDato1)>getProvincia(*(Sucursal*)ptrDato2))
     {
         return MAYOR;
     }else
-    if (getProvincia(*(Sucursal*)ptrDato1)!=getProvincia(*(Sucursal*)ptrDato2))
+    if (getProvincia(*(Sucursal*)ptrDato1)<getProvincia(*(Sucursal*)ptrDato2))
     {
         return MENOR;
     }
@@ -56,6 +56,19 @@ ResultadoComparacion compararIdProvincia(PtrDato ptrDato1, PtrDato ptrDato2) {
     else
         return IGUAL;
 }
+
+
+ResultadoComparacion compararMt2Fact(PtrDato ptrDato1, PtrDato ptrDato2) {
+    float cos1=getCm2(*(Sucursal*)ptrDato1)/ getMonto(*(Sucursal*)ptrDato1);
+    float cos2=getCm2(*(Sucursal*)ptrDato2)/ getMonto(*(Sucursal*)ptrDato2);
+    if (cos1 > cos2)
+        return MAYOR;
+    else if (cos1<cos2)
+        return MENOR;
+    else
+        return IGUAL;
+}
+
 void listasProvincias(Lista &listaSuc, Lista &listaProv, Lista listaOrdenProvinciasMonto)
 {
     PtrNodoLista ptrCursorP = primero(listaProv);
@@ -102,6 +115,13 @@ int main()
     cout<< "Ranking provincial por articulo"<<endl;
     cout<< ""<<endl;
     setCompare(listaSuc,compararArticuloProv);
+    reordenar(listaSuc);
+    ImprimirLista(listaSuc);
+
+    cout<< ""<<endl;
+    cout<< "Ranking asdasldjalsd mt2"<<endl;
+    cout<< ""<<endl;
+    setCompare(listaSuc,compararMt2Fact);
     reordenar(listaSuc);
     ImprimirLista(listaSuc);
 
