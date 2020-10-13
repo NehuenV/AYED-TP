a6ea52e5a77a0bbaab38b76492bbd0a19a7f7512
nashimz <miguelshakur97@gmail.com>
2020-10-12 21:01:00 -0300

    Codigo terminado posta posta

--- TPAYED-UNLA/TPAYED-UNLA/main.cpp
+++ TPAYED-UNLA/TPAYED-UNLA/main.cpp
@@ -8,41 +8,10 @@
 using namespace std;
 
 int opc;
-void modificacion(), consulta(), menu(),facturacion(),ventas(), rendimiento(), volver();
-void menu(){
-system("cls");
-    cout <<"###############################################"<< endl;
-    cout << "#\tSistema de control se sucursal.       #" << endl;
-    cout <<"###############################################"<< endl;
-    cout << "\n\n1. Modificacion" << endl;
-    cout << "\n2. Consulta" << endl;
-    cout << "\n3. Salir" << endl;
-    cout << "\n\nIngrese la opcion deseada: "; cin>>opc;
-    switch(opc){
-    case 1: modificacion(); break;
-    case 2: consulta(); break;
-    case 3: break;
-    default : menu();
-    }
-}
-void modificacion(){
-    system("cls");
-cout <<"###############################################"<< endl;
-    cout << "#\tSistema de control se sucursal.       #" << endl;
-    cout <<"###############################################"<< endl;
-    cout << "\n\n1. Alta sucursal" << endl;
-    cout << "\n2. Modificacion sucursal" << endl;
-    cout << "\n3. Baja sucursal" << endl;
-    cout << "\n4. Volver" << endl;
-    cout << "\n\nIngrese la opcion deseada: "; cin>>opc;
-    switch(opc){
-    case 1: modificacion(); break;
-    case 2: modificacion(); break;
-    case 3: modificacion(); break;
-    case 4: menu(); break;
-    default : modificacion();
-    }
-}
+void consulta(), menu(),facturacion(),ventas(), rendimiento(), volver();
+
+
+
 void consulta(){
 system("cls");
 cout <<"###############################################"<< endl;
@@ -51,14 +20,16 @@ cout <<"###############################################"<< endl;
     cout << "\n\n1. Sucursal con mayor facturacion" << endl;
     cout << "\n2. Sucursal con mayor Venta de Articulos" << endl;
     cout << "\n3. Sucursal con mayor rendimiento" << endl;
-    cout << "\n4. Volver" << endl;
+    cout << "\n4. Salir del programa" << endl;
+
     cout << "\n\nIngrese la opcion deseada: "; cin>>opc;
     system("cls");
     switch(opc){
     case 1: facturacion(); break;
     case 2: ventas(); break;
     case 3: rendimiento(); volver(); break;
-    case 4: menu(); break;
+    case 4: break;
+
     default : consulta();
     }
     }
@@ -120,7 +91,7 @@ cout << "\n Ingrese 0 para volver o cualquier numero para salir: "; cin>>opc;
 int main()
 {
 
-    menu();
+    consulta();
 
 
     return 0;
